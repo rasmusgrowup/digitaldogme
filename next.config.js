0d0aedf48e4c6d9c8135a5e3849988b38cbd6763
Rasmus Andersen <rasmus@growupstudio.dk>
2022-06-17 10:31:20 +0200

    redirects

--- next.config.js
+++ next.config.js
@@ -4,6 +4,60 @@ const nextConfig = {
   images: {
     domains: ['media.graphassets.com', 'media.graphcms.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/hvem-er-vi',
+        destination: '/om-os',
+        permanent: true,
+      },
+      {
+        source: '/digital-upskilling-landscape',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/hvem-er-vi/medarbejdere',
+        destination: '/om-os',
+        permanent: true,
+      },
+      {
+        source: '/hvem-er-vi/bestyrelsen',
+        destination: '/om-os',
+        permanent: true,
+      },
+      {
+        source: '/viden-ressourcer',
+        destination: '/viden',
+        permanent: true,
+      },
+      {
+        source: '/medlemskab',
+        destination: '/bliv-medlem',
+        permanent: true,
+      },
+      {
+        source: '/tilmelding-til-danmarks-digitale-kompetencer',
+        destination: '/om-os',
+        permanent: true,
+      },
+      {
+        source: '/tilmel-dig-vores-nyhedsbrev',
+        destination: '/om-os',
+        permanent: true,
+      },
+      {
+        source: '/tilmel-dig-vores-nyhedsbrev',
+        destination: '/om-os',
+        permanent: true,
+      },
+      {
+        source: '/generalforsamlinger',
+        destination: '/om-os',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
