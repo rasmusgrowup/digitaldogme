336b786a3b2a88c4719b0b582fe1d37bce23fa54
Rasmus Andersen <rasmus@growupstudio.dk>
2022-12-20 08:37:56 +0100

    workerThreads experimental feature

--- next.config.js
+++ next.config.js
@@ -2,79 +2,83 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['media.graphassets.com', 'media.graphcms.com'],
+    domains: ['media.graphassets.com', 'media.graphcms.com']
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1
   },
   async redirects() {
     return [
       {
         source: '/hvem-er-vi',
         destination: '/om-os',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/digital-upskilling-landscape',
         destination: '/',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/hvem-er-vi/medarbejdere',
         destination: '/om-os',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/hvem-er-vi/bestyrelsen',
         destination: '/om-os',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/viden-ressourcer',
         destination: '/viden',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/medlemskab',
         destination: '/bliv-medlem',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/tilmelding-til-danmarks-digitale-kompetencer',
         destination: '/om-os',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/tilmel-dig-vores-nyhedsbrev',
         destination: '/om-os',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/tilmel-dig-vores-nyhedsbrev',
         destination: '/om-os',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/generalforsamlinger',
         destination: '/om-os',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/viden/tryg-gar-efter-skyen-og-medarbejderne-skal-med',
         destination: '/cases/tryg-gar-efter-skyen-og-medarbejderne-skal-med',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/viden/opkvalificering-bor-tages-seriost',
         destination: '/cases/opkvalificering-bor-tages-seriost',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/viden/koncern-it',
         destination: '/cases/koncern-it',
-        permanent: true,
+        permanent: true
       },
       {
         source: '/viden/kobenhavns-lufthavn',
         destination: '/cases/kobenhavns-lufthavn',
-        permanent: true,
+        permanent: true
       }
     ]
   }
