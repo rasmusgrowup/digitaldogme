82022a6daf45ebe3d3a034580850dfe0d7f1e710
Rasmus Andersen <rasmus@growupstudio.dk>
2023-04-27 10:35:24 +0200

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,18 @@ import '../styles/globals.css'
 
 // Components
 import Layout from '../components/Layout'
-import { MenuProvider } from "../lib/menuContext"
+import {MenuProvider} from "../lib/menuContext"
+import {Analytics} from '@vercel/analytics/react';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <MenuProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </MenuProvider>
-  )
+function MyApp({Component, pageProps}) {
+    return (
+        <MenuProvider>
+            <Layout>
+                <Component {...pageProps} />
+                <Analytics />
+            </Layout>
+        </MenuProvider>
+    )
 }
 
 export default MyApp
