9f3ce012090f2caf433d8a288bdfccba20a2ab41
Rasmus Andersen <rasmus@growupstudio.dk>
2023-06-30 12:00:57 +0200

    first commit on grid section

--- components/Grid.js
+++ components/Grid.js
@@ -3,26 +3,28 @@ import Image from "next/image";
 
 export default function Grid({props}) {
     return (
-        <section className={styles.section}>
-            <div className={styles.inner}>
-                <h2 className={styles.heading}>{props.gridHeading}</h2>
-                <div className={styles.columns}>
-                    {props.columns && props.columns.map((column, i) => (
-                        <div key={i} className={styles.column}>
-                            <div className={styles.imageContainer}
-                                 style={{backgroundColor: column.columnImage.backgroundColor.css}}>
-                                <Image
-                                    src={column.columnImage.url}
-                                    alt={column.columnImage.alt}
-                                    height={column.columnImage.height}
-                                    width={column.columnImage.width}
-                                />
+        <>
+            <section className={styles.section}>
+                <div className={styles.inner}>
+                    <h2 className={styles.heading}>{props.gridHeading}</h2>
+                    <div className={styles.columns}>
+                        {props.columns && props.columns.map((column, i) => (
+                            <div key={i} className={styles.column}>
+                                <div className={styles.imageContainer}
+                                     style={{backgroundColor: column.columnImage.backgroundColor.css}}>
+                                    <Image
+                                        src={column.columnImage.url}
+                                        alt={column.columnImage.alt}
+                                        height={column.columnImage.height}
+                                        width={column.columnImage.width}
+                                    />
+                                </div>
+                                <p className={styles.imageDescription}>{column.imageDescription}</p>
                             </div>
-                            <p className={styles.imageDescription}>{column.imageDescription}</p>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
