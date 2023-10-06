0297f3708eee245e7c56db4400cd55aa0cebbc29
Rasmus Andersen <rasmus@growupstudio.dk>
2023-10-06 16:38:11 +0200

    major release

--- components/Grid.js
+++ components/Grid.js
@@ -42,7 +42,7 @@ export default function Grid({props, index, shouldHaveLine}) {
             <section className={`${theme === 'dark' ? `${styles.dark}` : theme === 'sky' ? `${styles.sky}` : theme === 'blue' ? `${styles.blue}` : theme === 'light' ? `${styles.light}` : theme === 'curry' ? `${styles.curry}` : theme === 'turquoise' ? `${styles.turquoise}` : theme === 'grey' ? `${styles.grey}` : theme === 'green' ? `${styles.green}` : theme === 'sand' ? `${styles.sand}` : `${styles.bg}`}`} id={props.id}
                      style={{scrollMarginTop: '50px'}}>
                 <div className={styles.inner}>
-                    <header className={styles.header} style={index === 0 || props.gridTheme !== null || !shouldHaveLine ? {border: 'none'} :  {borderTop: '1px solid var(--main)'}}>
+                    {props.gridHeading && <header className={styles.header} style={index === 0 || props.gridTheme !== null || !shouldHaveLine ? {border: 'none'} :  {borderTop: '1px solid var(--main)'}}>
                         {props.gridHeading &&
                             <h2 className={styles.h2}>
                                 {props.gridHeading}
@@ -58,7 +58,7 @@ export default function Grid({props, index, shouldHaveLine}) {
                                 </Link>
                             }
                         </div>
-                    </header>
+                    </header>}
                     {props.columns.length > 0 && <div className={props.stretchColumns ? `${styles.columns} ${styles.stretch}` : `${styles.columns}`}>
                         {props.columns && props.columns.map((column, i) => (
                             <div key={i} className={
